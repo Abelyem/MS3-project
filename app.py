e834468ea53f1d1d98f3e99ac33ffde7f75a7304
Abelyem <abel.yemane1@hotmail.co.uk>
2020-10-26 11:10:49 +0000

    Added API link to app.py (not yet functioning) / created new templates file called results.html where search results from API will later be displayed / created new route for search function (labelled /search)

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
 import os
-from flask import Flask
+import requests
+import cgi
+from flask import (
+    Flask, flash, render_template,
+    redirect, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
-def hello():
-    return "hello world"
+def index():
+    return render_template("base.html")
+
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    form = cgi.FieldStorage()
+    searchterm = form.getvalue('searchbox')
+    URL = "https://www.googleapis.com/books/v1/volumes?q=" + searchterm
+    r = requests.get(url=URL)
+    data = r.json()
+    return render_template("results.html", data=data)
 
 
 if __name__ == "__main__":
